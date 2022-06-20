5890b6c265ffa25855d8e260de8eee7dbb802460
Conrado1212 <conrado1212@gmail.com>
2022-06-20 20:03:57 +0200

    add js function which start and chceck answear

---
+++ js/scriptek.js
@@ -0,0 +1,28 @@
+var score = parseInt(0);
+var question = parseInt(0);
+var block;
+var correctJava =[1,1,1];
+var corJava = [];
+var qes;
+
+
+
+function start(){
+    document.getElementById("start-btn").style.display ="none";
+    document.getElementById("question0").style.display ="block";
+}
+
+function check(btnId){
+    for(var i=0;i<4;i++){
+        if(document.forms[btnId].elements[i].checked){
+            corJava[btnId]=i;
+        }
+    }
+    block = "correct" + btnId;
+    if(corJava[btnId] == correctJava[btnId]){
+        document.getElementById(correct).style.visibility ="visibile";
+        document.getElementById(correct).innerHTML = "Correct !!!";
+    }else{
+        document.getElementById(correct).innerHTML = "Wrong !!!";
+    }
+}
